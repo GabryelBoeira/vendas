f2f137a4dc7c22d7611356ee4614dd773506b82f
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-04-08 11:36:42 -0300

    trabalhando com múltiplos ambientes

---
+++ src/main/java/br/com/gabryel/vendas/config/ConfigDevelopment.java
@@ -0,0 +1,19 @@
+package br.com.gabryel.vendas.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("development")
+public class ConfigDevelopment {
+
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("Modo selecionado de desenvolvimento");
+        };
+    }
+}
