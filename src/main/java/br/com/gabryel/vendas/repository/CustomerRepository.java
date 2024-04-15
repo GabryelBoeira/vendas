8e639c84a99907cd5827739c49bcfe182d1ebb75
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-04-14 22:50:27 -0300

    finalizando CRUD em JdbcTemplate

--- src/main/java/br/com/gabryel/vendas/repository/CustomerRepository.java
+++ src/main/java/br/com/gabryel/vendas/repository/CustomerRepository.java
@@ -9,8 +9,11 @@ import java.util.List;
 @Repository
 public class CustomerRepository {
 
-	private static final String SAVE_CUSTOMER = "insert into CLIENTE (nome) values (?)"; // String
-	private static final String SELECT_CUSTOMER = "select * from CLIENTE";
+	private static final String SAVE_CUSTOMER = "INSERT INTO CLIENTE (nome) values (?)"; // String
+	private static final String SELECT_ALL_CUSTOMER = "SELECT * FROM CLIENTE";
+	private static final String UPDATE_CUSTOMER = "UPDATE CLIENTE SET nome = ? WHERE id = ?";
+	private static final String DELETE_CUSTOMER = "DELETE FROM CLIENTE where id = ?";
+
 
 	private final JdbcTemplate jdbcTemplate;
 
@@ -24,7 +27,7 @@ public class CustomerRepository {
 	 * @return the customer information
 	 */
 	public List<Customer> findAllCustomer() {
-		return jdbcTemplate.query(SELECT_CUSTOMER,
+		return jdbcTemplate.query(SELECT_ALL_CUSTOMER,
 			(rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("nome")));
 	}
 
@@ -38,5 +41,41 @@ public class CustomerRepository {
 		return customer;
 	}
 
+	public Customer updateCustomer(Customer customer) {
+		jdbcTemplate.update(UPDATE_CUSTOMER, new Object[]{customer.getName(), customer.getId()});
+		return customer;
+	}
+
+	/**
+	 * Deletes a customer from the database.
+	 *
+	 * @param  customer	The customer object to be deleted
+	 * @return         	void
+	 */
+	public void deleteCustomer(Customer customer) {
+		deleteCustomerById(customer.getId());
+	}
+
+	/**
+	 * Deletes a customer from the database by ID.
+	 *
+	 * @param  customerId The customer ID to be deleted
+	 * @return         	void
+	 */
+	public void deleteCustomerById(Integer customerId) {
+		jdbcTemplate.update(DELETE_CUSTOMER, new Object[] {customerId});
+	}
+
+	/**
+	 * Find customers by name.
+	 *
+	 * @param  name	 find customer name by like
+	 * @return        list for customers
+	 */
+	public List<Customer> findCustomerByName(String	name) {
+		return jdbcTemplate.query(SELECT_ALL_CUSTOMER.concat(" WHERE nome like ?"),
+				new Object[]{"%" + name + "%"},
+				(rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("nome")));
+	}
 
 }
