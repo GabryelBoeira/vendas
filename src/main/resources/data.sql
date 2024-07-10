40f5e02bf79305324c39614b32b6f9d45e98c718
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-10 20:24:53 -0300

    criação de pedido no sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 --- ADD CLIENTES
-insert into `CLIENTE` (`NOME`) values ('João');
+insert into `CLIENTE` (`ID`,`NOME`) values (100,'João');
 insert into `CLIENTE` (`NOME`) values ('Maria');
 insert into `CLIENTE` (`NOME`) values ('Jose');
 insert into `CLIENTE` (`NOME`) values ('Carlos');
@@ -7,8 +7,23 @@ insert into `CLIENTE` (`NOME`) values ('Pedro');
 insert into `CLIENTE` (`NOME`) values ('Will Smith');
 
 --- ADD PRODUTOS
-insert into `PRODUTO` (`DESCRICAO`, `VALOR`) values ('Cerveja', 5.00);
-insert into `PRODUTO` (`DESCRICAO`, `VALOR`) values ('Agua', 2.00);
-insert into `PRODUTO` (`DESCRICAO`, `VALOR`) values ('Refrigerante', 3.00);
-insert into `PRODUTO` (`DESCRICAO`, `VALOR`) values ('Suco', 3.00);
-insert into `PRODUTO` (`DESCRICAO`, `VALOR`) values ('Cachorro Quente', 4.00);
+insert into `PRODUTO` (`ID`, `DESCRICAO`, `VALOR`) values (1, 'Cerveja', 5.00);
+insert into `PRODUTO` (`ID`, `DESCRICAO`, `VALOR`) values (2, 'Agua', 2.00);
+insert into `PRODUTO` (`ID`, `DESCRICAO`, `VALOR`) values (3, 'Refrigerante', 3.00);
+insert into `PRODUTO` (`ID`, `DESCRICAO`, `VALOR`) values (4, 'Suco', 3.00);
+insert into `PRODUTO` (`ID`, `DESCRICAO`, `VALOR`) values (5, 'Cachorro Quente', 4.00);
+
+
+---ADD pedidos
+insert into `PEDIDO` (`ID`, `CLIENTE_ID`, `VALOR_TOTAL`, `DATA_PEDIDO`) values (1, 100, 150.00, '2020-01-01 00:00:00.000000');
+insert into `PEDIDO` (`ID`, `CLIENTE_ID`, `VALOR_TOTAL`, `DATA_PEDIDO`) values (2, 100, 100.00, '2020-01-01 00:00:00.000000');
+insert into `PEDIDO` (`ID`, `CLIENTE_ID`, `VALOR_TOTAL`, `DATA_PEDIDO`) values (3, 100, 100.00, '2020-01-01 00:00:00.000000');
+
+---ADD item pedido
+insert into `PEDIDO_ITEM` (`PEDIDO_ID`, `PRODUTO_ID`, `QUANTIDADE`) values (1, 1, 2);
+insert into `PEDIDO_ITEM` (`PEDIDO_ID`, `PRODUTO_ID`, `QUANTIDADE`) values (1, 5, 1);
+insert into `PEDIDO_ITEM` (`PEDIDO_ID`, `PRODUTO_ID`, `QUANTIDADE`) values (1, 3, 2);
+
+insert into `PEDIDO_ITEM` (`PEDIDO_ID`, `PRODUTO_ID`, `QUANTIDADE`) values (2, 3, 5);
+insert into `PEDIDO_ITEM` (`PEDIDO_ID`, `PRODUTO_ID`, `QUANTIDADE`) values (2, 4, 2);
+insert into `PEDIDO_ITEM` (`PEDIDO_ID`, `PRODUTO_ID`, `QUANTIDADE`) values (2, 5, 7);
