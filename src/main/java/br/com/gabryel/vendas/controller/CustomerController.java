c5d1f704abbe975192ba0a0f65e5f906ec210bf5
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-14 16:17:50 -0300

    Update CustomerController.java

--- src/main/java/br/com/gabryel/vendas/controller/CustomerController.java
+++ src/main/java/br/com/gabryel/vendas/controller/CustomerController.java
@@ -6,11 +6,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/customer")
@@ -27,14 +34,14 @@ public class CustomerController {
     @Operation(summary = "Cria um novo cliente")
     @PostMapping
     public CustomerDTO createCustomer(@RequestBody @Valid CustomerDTO customer) {
-        return new CustomerDTO(1, "will smith", new ArrayList<>());
+        return customerService.saveCustomerDTO(customer);
     }
 
     @Operation(summary = "Busca o cliente pelo ID")
     @GetMapping("/{id}")
     public ResponseEntity<Object> findCustomerById(@PathVariable Integer id) {
         try {
-            return ResponseEntity.ok(customerService.findCustomerAndPurchaseOrderById(id));
+            return ResponseEntity.ok(customerService.findCustomerById(id));
         } catch (Exception e) { 
             return ResponseEntity.notFound().build();
         }
@@ -43,13 +50,20 @@ public class CustomerController {
     @Operation(summary = "Busca o cliente pelo nome")
     @GetMapping("/find/name")
     public CustomerDTO findCustomerByName(@RequestParam String name) {
-        return new CustomerDTO(1, "will smith", new ArrayList<>());
+        CustomerDTO customer = customerService.findCustomerByName(name);
+        if (customer != null) return customer;
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found: " + name);
     }
 
     @Operation(summary = "Atualizar dados do cliente")
     @PutMapping
-    public CustomerDTO updateCustomer(@RequestBody @Valid CustomerDTO customer) {
-        return new CustomerDTO(1, "will smith", new ArrayList<>());
+    public ResponseEntity<Object> updateCustomer(@RequestBody @Valid CustomerDTO customer) {
+        try {
+            return ResponseEntity.ok(customerService.updateCustomer(customer));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Operation(summary = "Deletar o cliente pelo ID")
@@ -57,5 +71,7 @@ public class CustomerController {
     public ResponseEntity<Object> deleteCustomer(@PathVariable Integer id) {
         return ResponseEntity.ok().build();
     }
+
+
 
 }
