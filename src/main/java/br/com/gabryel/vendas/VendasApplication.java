71ab3377523a0dfe61d147a632c14873356660b6
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-22 19:17:01 -0300

    configurando projeto para build
    build da aplicação em formato war

--- src/main/java/br/com/gabryel/vendas/VendasApplication.java
+++ src/main/java/br/com/gabryel/vendas/VendasApplication.java
@@ -1,17 +1,12 @@
 package br.com.gabryel.vendas;
 
 
-import br.com.gabryel.vendas.entity.Customer;
-import br.com.gabryel.vendas.repository.otherVersions.CustomerJdbcRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
