dbf3570bd556346dbc58aa33ac6724bc9ec7be51
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-23 19:47:34 -0300

    adicionando mais cidades

--- localizacao/src/main/resources/data.sql
+++ localizacao/src/main/resources/data.sql
@@ -5,6 +5,16 @@ create table if not exists tb_cidades (
     PRIMARY KEY ( ID )
 );
 
-insert into tb_cidades (ID, NOME, POPULACAO) values (1, 'Campinas', 200000);
-insert into tb_cidades (ID, NOME, POPULACAO) values (2, 'Sao Paulo', 300000);
-insert into tb_cidades (ID, NOME, POPULACAO) values (3, 'Rio de Janeiro', 400000);
+insert into tb_cidades
+    (ID, NOME, POPULACAO)
+values
+    (1, 'Campinas', 200000),
+    (2, 'Sao Paulo', 30000000),
+    (3, 'Rio de Janeiro', 400000),
+    (4, 'Belo Horizonte', 500000),
+    (5, 'Porto Alegre', 600000),
+    (6, 'Florianopolis', 700000),
+    (7, 'Curitiba', 800000),
+    (8, 'Recife', 900000),
+    (9, 'Fortaleza', 1000000),
+    (10, 'Natal', 1100000);
