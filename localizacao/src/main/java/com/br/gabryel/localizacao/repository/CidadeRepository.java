516c53af47f6ccac66fd20d7cc8216dfb59df338
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-23 22:22:42 -0300

    Utilizando variações para consulta com string
    Utilizando camelCase de consulta jpa para realizar consultas sem @query

--- localizacao/src/main/java/com/br/gabryel/localizacao/repository/CidadeRepository.java
+++ localizacao/src/main/java/com/br/gabryel/localizacao/repository/CidadeRepository.java
@@ -4,7 +4,42 @@ import com.br.gabryel.localizacao.entity.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 
+    /**
+     * Finds a list of Cidade objects by their nome.
+     *
+     * @param  nome	the name to search for
+     * @return         	a list of Cidade objects with the given name
+     */
+    List<Cidade> findByNome(String nome);
+
+    /**
+     * Finds a list of Cidade objects by their nome starting with the given string.
+     *
+     * @param  nome     the string to search for in the nome field
+     * @return          a list of Cidade objects with the given nome starting with the provided string
+     */
+    List<Cidade> findByNomeStartingWith(String nome);
+
+    /**
+     * Finds a list of Cidade objects by their nome ending with the given string.
+     *
+     * @param  nome     the string to search for in the nome field
+     * @return          a list of Cidade objects with the given nome ending with the provided string
+     */
+    List<Cidade> findByNomeEndingWith(String nome);
+
+    /**
+     * Finds a list of Cidade objects by their nome containing the given string.
+     *
+     * @param  nome     the string to search for in the nome field
+     * @return          a list of Cidade objects with the given nome containing the provided string
+     */
+    List<Cidade> findByNomeContaining(String nome);
+
+    
 }
