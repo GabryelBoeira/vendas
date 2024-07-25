63ad8009f96b861f0d1ddee3212ca8effea576b1
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-25 15:47:57 -0300

    Exemplo de consulta numérica jpa

--- localizacao/src/main/java/com/br/gabryel/localizacao/repository/CidadeRepository.java
+++ localizacao/src/main/java/com/br/gabryel/localizacao/repository/CidadeRepository.java
@@ -2,6 +2,7 @@ package com.br.gabryel.localizacao.repository;
 
 import com.br.gabryel.localizacao.entity.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,6 +17,9 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long> {
      * @return         	a list of Cidade objects with the given name
      */
     List<Cidade> findByNome(String nome);
+
+    @Query("select c from Cidade c where upper(c.nome) like upper(?1)")
+    List<Cidade> findByNomeLike(String nome);
 
     /**
      * Finds a list of Cidade objects by their nome starting with the given string.
@@ -41,5 +45,45 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long> {
      */
     List<Cidade> findByNomeContaining(String nome);
 
-    
+    /**
+     * Finds a list of Cidade objects by their populacao.
+     *
+     * @param  populacao  the populacao to search for
+     * @return             a list of Cidade objects with the given populacao
+     */
+    List<Cidade> findByPopulacao(Long populacao);
+
+    /**
+     * Finds a list of Cidade objects with a population greater than the given population.
+     *
+     * @param  populacao  the population to compare against
+     * @return             a list of Cidade objects with a population greater than the given population
+     */
+    List<Cidade> findByPopulacaoGreaterThan(Long populacao);
+
+    /**
+     * Finds a list of Cidade objects with a population less than the given population.
+     *
+     * @param  populacao  the population to compare against
+     * @return             a list of Cidade objects with a population less than the given population
+     */
+    List<Cidade> findByPopulacaoLessThan(Long populacao);
+
+    /**
+     * Finds a list of Cidade objects with a population less than or equal to the given population.
+     *
+     * @param  populacao  the population to compare against
+     * @return             a list of Cidade objects with a population less than or equal to the given population
+     */
+    List<Cidade> findByPopulacaoLessThanEqual(Long populacao);
+
+    /**
+     * Finds a list of Cidade objects with a population between the given minimum and maximum values.
+     *
+     * @param  populacaoMin  the minimum population value to search for
+     * @param  populacaoMax  the maximum population value to search for
+     * @return                a list of Cidade objects with a population between the given minimum and maximum values
+     */
+    List<Cidade> findByPopulacaoBetween(Long populacaoMin, Long populacaoMax);
+
 }
