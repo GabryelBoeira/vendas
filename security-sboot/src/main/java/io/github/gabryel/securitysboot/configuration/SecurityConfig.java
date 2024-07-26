c093eb79dfcc7dcbfcc5e909c7faa127ef153c2f
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-26 20:18:04 -0300

    configuração básica do WebSecurity

---
+++ security-sboot/src/main/java/io/github/gabryel/securitysboot/configuration/SecurityConfig.java
@@ -0,0 +1,28 @@
+package io.github.gabryel.securitysboot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/public").permitAll()
+                        .requestMatchers("/private").authenticated()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
+                .build();
+    }
+
+
+}
