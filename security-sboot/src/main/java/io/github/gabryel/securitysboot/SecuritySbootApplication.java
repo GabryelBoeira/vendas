dfafb894d03047cefd6860607e5d5e8e53dfcf2a
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-25 22:26:16 -0300

    commit inicial do projeto

---
+++ security-sboot/src/main/java/io/github/gabryel/securitysboot/SecuritySbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabryel.securitysboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySbootApplication.class, args);
+    }
+
+}
---
+++ security-sboot/src/test/java/io/github/gabryel/securitysboot/SecuritySbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabryel.securitysboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
