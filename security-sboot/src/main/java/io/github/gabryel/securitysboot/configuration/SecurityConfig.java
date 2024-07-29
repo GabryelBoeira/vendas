fd6abd7ee95c2223cef987fb11d104fe90600b94
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-28 21:03:26 -0300

    Configurando credenciais de acesso

--- security-sboot/src/main/java/io/github/gabryel/securitysboot/configuration/SecurityConfig.java
+++ security-sboot/src/main/java/io/github/gabryel/securitysboot/configuration/SecurityConfig.java
@@ -1,13 +1,18 @@
 package io.github.gabryel.securitysboot.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -23,6 +28,29 @@ public class SecurityConfig {
                 .logout(Customizer.withDefaults())
                 .build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = org.springframework.security.core.userdetails.User
+                .withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = org.springframework.security.core.userdetails.User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("USER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 
 
 }
