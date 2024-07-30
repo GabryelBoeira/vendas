23dedfc2f8e5d86dffe44fc1d9b061d82e46c3fd
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-07-29 21:07:53 -0300

    Criptografando a senha
    Testando o fluxo de cadastro de usuario e permissoes

--- security-sboot/src/main/java/io/github/gabryel/securitysboot/controller/UsuarioController.java
+++ security-sboot/src/main/java/io/github/gabryel/securitysboot/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import io.github.gabryel.securitysboot.entity.Usuario;
 import io.github.gabryel.securitysboot.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,9 @@ public class UsuarioController {
     private final UsuarioService service;
 
     @PostMapping
-    public ResponseEntity<Usuario> salvar(@RequestBody UsuarioDTO usuario) {
-        return ResponseEntity.ok().body(service.salvar(usuario));
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Usuario> salvar(@RequestBody UsuarioDTO usuario) throws Exception {
+        return ResponseEntity.ok().body(service.salvar(usuario.usuario(), usuario.permissoes()));
     }
 
 }
--- security-sboot/src/main/java/io/github/gabryel/securitysboot/service/UsuarioService.java
+++ security-sboot/src/main/java/io/github/gabryel/securitysboot/service/UsuarioService.java
@@ -1,10 +1,11 @@
 package io.github.gabryel.securitysboot.service;
 
-import io.github.gabryel.securitysboot.dto.UsuarioDTO;
 import io.github.gabryel.securitysboot.entity.GrupoUsuario;
 import io.github.gabryel.securitysboot.entity.Usuario;
 import io.github.gabryel.securitysboot.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,13 +19,18 @@ public class UsuarioService {
     private final UsuarioRepository repository;
     private final GrupoService grupoService;
     private final GrupoUsuarioService grupoUsuarioService;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public Usuario salvar(UsuarioDTO usuarioDto) {
-        var usuario = repository.save(usuarioDto.usuario());
+    public Usuario salvar(Usuario usuario, List<String> permissoes) throws Exception {
+        if(StringUtils.isBlank(usuario.getPassword())) {
+            throw new Exception("Senha e obrigatoria");
+        }
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+        var usuarioSalvo = repository.save(usuario);
 
         List<GrupoUsuario> grupos = new ArrayList<>();
-        usuarioDto.permissoes()
+        permissoes
                 .forEach(permissao -> {
                     var grupo = grupoService.findByNome(permissao);
                     if (grupo.isPresent())
@@ -32,7 +38,7 @@ public class UsuarioService {
                 });
         grupoUsuarioService.savarTodos(grupos);
 
-        return usuario;
+        return usuarioSalvo;
     }
 
 }
